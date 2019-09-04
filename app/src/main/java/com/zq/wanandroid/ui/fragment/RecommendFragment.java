905b465641df74ef16296b03960e3a959723d739
zhangqi <969860880@qq.com>
2019-09-04 13:28:04 +0800

    add SmartRefreshLayout

--- app/src/main/java/com/zq/wanandroid/ui/fragment/RecommendFragment.java
+++ app/src/main/java/com/zq/wanandroid/ui/fragment/RecommendFragment.java
@@ -5,6 +5,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.zq.wanandroid.R;
 import com.zq.wanandroid.di.component.AppComponent;
 import com.zq.wanandroid.di.component.DaggerRecommendComponent;
@@ -23,10 +27,31 @@ public class RecommendFragment extends ProgressFragment<RecommedPresenter> imple
     private static final String TAG = RecommendFragment.class.getSimpleName();
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
+    @BindView(R.id.refresh_layout)
+    SmartRefreshLayout refreshLayout;
 
     @Override
     protected void init() {
         presenter.requestData();
+
+        initRefreshLayout();
+    }
+
+    private void initRefreshLayout() {
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                Log.e(TAG, "------------------onRefresh--------------------------- ");
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+            }
+        });
+
     }
 
     @Override
@@ -59,8 +84,9 @@ public class RecommendFragment extends ProgressFragment<RecommedPresenter> imple
         IndexMixAdapter indexMixAdapter = new IndexMixAdapter(activity, indexBean);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity));
         //                为RecyclerView设置分割线(这个可以对DividerItemDecoration进行修改，自定义)
-//        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        //        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(indexMixAdapter);
     }
+
 }
